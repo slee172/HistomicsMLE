4373aacd972199a72985d785fe3d3342c8b43629
slee172 <shlee8020@gmail.com>
2025-05-07 20:35:12 +0000

    Added hostspecs.php

---
+++ web_app/php/hostspecs.php
@@ -0,0 +1,12 @@
+<?php
+	# !!!!!
+	# !!!!! The one and ONLY place the active learning server is defined
+	# !!!!!
+	$port = 10000;
+	$host = "localhost";
+
+	#
+	#	IIP server
+	#
+	$IIPServer = "http://localhost/iipsrv/iipsrv.fcgi?";
+?>
